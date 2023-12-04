b9cb76909d6bb62ead9b5ee1c7298129f8605a21
nata757 <107883642+nata757@users.noreply.github.com>
2023-12-04 18:32:56 +0100

    Added LoginTest

---
+++ src/test/java/e2e/TestBase.java
@@ -0,0 +1,14 @@
+package e2e;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class TestBase {
+    protected static ApplicationManager app = new ApplicationManager();
+    @BeforeTest
+    public void setAppTest(){app.init();}
+    @AfterTest
+    public void tearDown(){
+        app.stop();
+    }
+}
---
+++ src/test/java/e2e/pages/InventoryPage.java
@@ -0,0 +1,15 @@
+package e2e.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InventoryPage extends BasePage {
+    public WebDriver driver;
+
+
+    public InventoryPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
