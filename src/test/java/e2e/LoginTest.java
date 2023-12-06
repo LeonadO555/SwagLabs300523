7252d11e8cfac411c25959b32f80881695691110
Pislaru <pislaru.daniel1@gmail.com>
2023-12-06 17:22:36 +0100

    LoginTest for all UsersName

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -15,6 +15,42 @@ public class LoginTest extends TestBase {
         loginPage.login(email, password);
 
     }
+    @Test
+    public void userCanLogin1(){
+        String email = "problem_user";
+        String password = "secret_sauce";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email, password);
+
+    }
+    @Test
+    public void userCanLogin2(){
+        String email = "performance_glitch_user";
+        String password = "secret_sauce";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email, password);
+
+    }
+    @Test
+    public void userCanLogin3(){
+        String email = "error_user";
+        String password = "secret_sauce";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email, password);
+
+    }
+    @Test
+    public void userCanLogin4(){
+        String email = "visual_user";
+        String password = "secret_sauce";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email, password);
+
+    }
 
 
 }
