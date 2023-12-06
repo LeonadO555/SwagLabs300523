bc1709414a1cbe0c6bc7cfd41991cc5b79e534d9
Pislaru <pislaru.daniel1@gmail.com>
2023-12-06 16:59:03 +0100

    1 commit

---
+++ src/test/java/e2e/LoginTest.java
@@ -0,0 +1,21 @@
+package e2e;
+
+import e2e.pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase {
+    LoginPage loginPage;
+
+    @Test
+    public void userCanLogin(){
+        String email = "standard_user";
+        String password = "secret_sauce";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email, password);
+        loginPage.waitForLoading();
+
+    }
+
+
+}
