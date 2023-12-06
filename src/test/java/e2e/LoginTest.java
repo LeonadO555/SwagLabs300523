5151092342a3aa47685e38ab5a888b1cf3e56964
nata757 <107883642+nata757@users.noreply.github.com>
2023-12-06 20:03:35 +0100

    Added InventoryPage and LoginTest

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -8,26 +8,78 @@ public class LoginTest extends TestBase {
     LoginPage LoginPage;
 
     @Test
-    public void userCanLogin() {
-        String email = "standard_user";
+    public void userCanLoginWithValidStandardUserName() {
+        String userName = "standard_user";
         String password = "secret_sauce";
 
         LoginPage = new LoginPage(app.driver);
-
-        String userName = new String();
-
         LoginPage.login(userName, password);
         LoginPage.waitForLoading();
     }
 
     @Test
-    public void userCannotLoginWithInvalidEmail() {
+    public void userCannotLoginWithInvalidName() {
 
-        String email = "standarduser";
+        String userName = "standarduser";
         String password = "secret_sauce";
 
         LoginPage = new LoginPage(app.driver);
-        LoginPage.login(email, password);
+        LoginPage.login(userName, password);
+        LoginPage.waitForLoading();
+    }
+
+    @Test
+    public void userCanLoginWithValidLockedOutUserName() {
+
+        String userName = "locked_out_user";
+        String password = "secret_sauce";
+
+        LoginPage = new LoginPage(app.driver);
+        LoginPage.login(userName, password);
+        LoginPage.waitForLoading();
+    }
+
+    @Test
+    public void userCanLoginWithValidProblemUserName() {
+
+        String userName = "problem_user";
+        String password = "secret_sauce";
+
+        LoginPage = new LoginPage(app.driver);
+        LoginPage.login(userName, password);
+        LoginPage.waitForLoading();
+    }
+
+    @Test
+    public void userCanLoginWithValidPerformanceGlitchUserName() {
+
+        String userName = "performance_glitch_user";
+        String password = "secret_sauce";
+
+        LoginPage = new LoginPage(app.driver);
+        LoginPage.login(userName, password);
+        LoginPage.waitForLoading();
+    }
+
+    @Test
+    public void userCanLoginWithValidErrorUserName() {
+
+        String userName = "error_user";
+        String password = "secret_sauce";
+
+        LoginPage = new LoginPage(app.driver);
+        LoginPage.login(userName, password);
+        LoginPage.waitForLoading();
+    }
+
+    @Test
+    public void userCanLoginWithValidVisualUserName() {
+
+        String userName = "visual_user";
+        String password = "secret_sauce";
+
+        LoginPage = new LoginPage(app.driver);
+        LoginPage.login(userName, password);
         LoginPage.waitForLoading();
     }
 }
