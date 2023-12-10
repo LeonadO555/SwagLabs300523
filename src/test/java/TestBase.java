3287981018afa4016aa7f2e44eb1552754082449
Tetiana Skvarnyk <t.skvarnikgmail.com>
2023-12-10 20:44:58 +0100

    Added some Classes

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+    protected  static ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod //выполняется до метода, кот запускаем
+    public void setupTest(){
+        app.init();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        app.stop();
+    }
+}
