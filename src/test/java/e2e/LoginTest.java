40b1626d25d4bf40b537754511f32d94e3836a60
Georgiy <g.manolov@web.de>
2023-12-10 08:46:43 +0100

    Data Provider

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -1,15 +1,16 @@
 package e2e;
 
 import e2e.pages.LoginPage;
+import e2e.util.DataProviders;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
     LoginPage loginPage;
 
-    @Test
-    public void userCanLogin(){
-        String email = "standard_user";
-        String password = "secret_sauce";
+    @Test(dataProvider = "userCanLogin",dataProviderClass = DataProviders.class)
+    public void userCanLogin(String email, String password){
+       // String email = "standard_user";
+       // String password = "secret_sauce";
 
         loginPage = new LoginPage(app.driver);
         loginPage.login(email, password);
