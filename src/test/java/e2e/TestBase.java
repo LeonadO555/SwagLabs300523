f08a5d8c6fe56cbf9711e14e0416f37ca7113739
nata757 <107883642+nata757@users.noreply.github.com>
2023-12-12 19:22:11 +0100

    Added class DataProviders

--- src/test/java/e2e/TestBase.java
+++ src/test/java/e2e/TestBase.java
@@ -1,9 +1,8 @@
 package e2e;
 
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+
 
 public class TestBase {
     protected static ApplicationManager app = new ApplicationManager();
