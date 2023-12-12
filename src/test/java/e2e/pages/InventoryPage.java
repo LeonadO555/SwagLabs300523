3d7541fcd5155018546e9db2972fa569d5751862
nata757 <107883642+nata757@users.noreply.github.com>
2023-12-12 14:51:30 +0100

    Added class DataProviders

--- src/test/java/e2e/pages/InventoryPage.java
+++ src/test/java/e2e/pages/InventoryPage.java
@@ -1,8 +1,11 @@
 package e2e.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import static sun.jvm.hotspot.interpreter.InterpreterCodelet.descriptionField;
 
 public class InventoryPage extends BasePage {
     public WebDriver driver;
@@ -11,8 +14,46 @@ public class InventoryPage extends BasePage {
         super(driver);
     }
 
-    @FindBy(xpath = "//*[@id='item_0_title_link']")
-    WebElement titleLink;
+    @FindBy(xpath = "//*[@id='react-burger-menu-btn']")
+    WebElement burgerMenuButton;
+
+    @FindBy(xpath = "//div[@class='bm-menu-wrap']")
+    WebElement menuWrapButton;
+    @FindBy(xpath = "//*[@id='logout_sidebar_link']")
+    WebElement logoutLinkButton;
+
+    //@FindBy(xpath = "//*[@id='item_0_title_link']")
+    //WebElement titleLinkButton;
+
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-backpack']")
+    WebElement addToCartSauceLabsBackpackButton;
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-bike-light']")
+    WebElement addToCartSauceLabsBikeLightButton;
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-bolt-t-shirt']")
+    WebElement addToCartSauceLabBoltTShirtsButton;
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-fleece-jacket']")
+    WebElement addToCartSauceLabsFleeceJacketButton;
+    @FindBy(xpath = "//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")
+    WebElement addToCartTShirtRedButton;
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-onesie']")
+    WebElement addToCartSauceLabsOneSizeButton;
 
 
+    public void waitForLoading() {
+        getWait().forVisibility(burgerMenuButton);
+        getWait().forVisibility(menuWrapButton);
+        getWait().forVisibility(logoutLinkButton);
+        getWait().forVisibility(addToCartSauceLabsBackpackButton);
+        getWait().forClickable(addToCartSauceLabsBikeLightButton);
+        getWait().forClickable(addToCartSauceLabBoltTShirtsButton);
+        getWait().forClickable(addToCartSauceLabsFleeceJacketButton);
+        getWait().forClickable(addToCartTShirtRedButton);
+        getWait().forClickable(addToCartSauceLabsOneSizeButton);
+    }
+    public void setBurgerMenuButton() {
+        burgerMenuButton.click();
+    }
+public void setLogoutLinkButton(){
+        logoutLinkButton.click();
+}
 }
