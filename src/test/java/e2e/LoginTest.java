94edb69500ad7ab0461fbcecbf0f4b95280369fa
Tetiana Skvarnyk <t.skvarnikgmail.com>
2023-12-26 13:46:33 +0100

    Added LoginTest.java confirmLogin Test

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -1,11 +1,13 @@
 package e2e;
 
 import e2e.Pages.LoginPage;
+import e2e.Pages.ProductsPage;
 import e2e.utils.DataProviders;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase{
     LoginPage loginPage;
+    ProductsPage productsPage;
 
 
     @Test(dataProvider = "userCanLogin", dataProviderClass = DataProviders.class)
@@ -14,7 +16,13 @@ public class LoginTest extends TestBase{
         loginPage.waitForLoading();
         loginPage.login(email, password);
 
+        productsPage = new ProductsPage(app.driver);
+        productsPage.waitForLoading();
+        productsPage.confirmLogin();
+        productsPage.waitForLoading();
+
 
     }
+
 
 }
