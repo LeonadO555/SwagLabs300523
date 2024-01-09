8037e2761fb579b379af25bd30b1d391a5452591
Tetiana Skvarnyk <t.skvarnikgmail.com>
2024-01-09 14:45:15 +0100

    Added ProductsPage.java

---
+++ src/test/java/e2e/Pages/ProductsPage.java
@@ -0,0 +1,24 @@
+package e2e.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsPage extends BasePage{
+    public ProductsPage(WebDriver driver) {super(driver);
+    }
+
+    @FindBy(xpath = "//*[@id='header_container']")
+    WebElement header;
+
+    public void waitForLoading(){
+        getWait().forVisibility(header);
+    }
+
+    public boolean confirmLogin(){
+        return header.isDisplayed();
+
+    }
+}
+
+
