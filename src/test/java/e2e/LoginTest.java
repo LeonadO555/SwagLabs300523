72435e858cf63a4494a5e4206af2762b6b1fca2c
Tetiana Skvarnyk <t.skvarnikgmail.com>
2024-01-09 14:56:16 +0100

    Added ProductsPage.java

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -1,21 +1,28 @@
 package e2e;
 
 import e2e.Pages.LoginPage;
+import e2e.Pages.ProductsPage;
 import e2e.utils.DataProviders;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase{
     LoginPage loginPage;
-
+    ProductsPage productsPage;
 
 
     @Test(dataProvider = "userCanLogin", dataProviderClass = DataProviders.class)
-        public void userCanLoginWithAllUsernames(String email, String password){
+    public void userCanLoginWithAllUsernames(String email, String password){
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
         loginPage.login(email, password);
 
+        productsPage = new ProductsPage(app.driver);
+        productsPage.waitForLoading();
+        productsPage.confirmLogin();
+        productsPage.waitForLoading();
+
 
     }
+
 
 }
